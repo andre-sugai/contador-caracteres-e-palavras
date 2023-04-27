985bfe904caf85194933c6cad453cadc600d3fb2
André Sugai <guitarfreaks@gmail.com>
2023-04-27 02:09:53 -0300

    adicionando javascript para contagem de caracteres e palavras

---
+++ script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const input = document.querySelector('#input');
+const counter = document.querySelector('.counter');
+const toggleButton = document.querySelector('#toggle');
+
+let mode = 'characters';
+
+toggleButton.addEventListener('click', () => {
+  if(mode === 'characters') {
+    mode = 'words';
+    toggleButton.textContent = 'Contar Caracteres';
+  } else {
+    mode = 'characters';
+    toggleButton.textContent = 'Contar Palavras';
+  }
+
+  input.dispatchEvent(new Event('input'));
+})
+
+input.addEventListener('input', () => {
+  let count = 0;
+  if(mode === 'characters') {
+    count = input.value.length;
+    counter.textContent = `${count} caractere(s)`;
+  } else {
+    const words = input.value.trim().split(/\s+/);
+    // console.log(input.value);
+    // console.log(words);
+    count = input.value.trim() === '' ? 0 : words.length;
+    counter.textContent = `${count} palavra(s)`;
+  }
+})
